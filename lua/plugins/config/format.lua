e14305c05a4b522ead63354c511901fbab94af43
Ramy Fakhoury <Ramy.fakhoury@vastdata.com>
2024-11-23 15:14:51 +0200

    conform enables auto foramt on save for just git hunks.

--- lua/plugins/config/format.lua
+++ lua/plugins/config/format.lua
@@ -1,4 +1,22 @@
 -- https://github.com/stevearc/conform.nvim
+vim.api.nvim_create_user_command("DisableAutoformat", function()
+  -- Disable autoformatting globally
+  vim.g.disable_autoformat = true
+  vim.notify("Global autoformatting disabled", vim.log.levels.INFO)
+end, {
+  desc = "Disable autoformatting globally or for the current buffer (use ! for buffer-only)",
+  bang = true,
+})
+
+vim.api.nvim_create_user_command("EnableAutoformat", function()
+  -- Enable autoformatting globally
+  vim.g.disable_autoformat = false
+  vim.notify("Global autoformatting enabled", vim.log.levels.INFO)
+end, {
+  desc = "Enable autoformatting globally or for the current buffer (use ! for buffer-only)",
+  bang = true,
+})
+
 M = {}
 
 M.setup_conform = function()
@@ -73,4 +91,59 @@ M.setup_conform = function()
   }) ]]
 end
 
+local function format_hunks()
+  local ignore_filetypes = {} --{ "lua" }
+  if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+    vim.notify("range formatting for " .. vim.bo.filetype .. " not working properly.")
+    return
+  end
+
+  local hunks = require("gitsigns").get_hunks()
+  if hunks == nil then
+    return
+  end
+
+  local format = require("conform").format
+
+  local function format_range()
+    if next(hunks) == nil then
+      print("done formatting! ")
+      -- vim.notify("done formatting git hunks", "info", { title = "formatting" })
+      return
+    end
+    local hunk = nil
+    while next(hunks) ~= nil and (hunk == nil or hunk.type == "delete") do
+      hunk = table.remove(hunks)
+    end
+
+    if hunk ~= nil and hunk.type ~= "delete" then
+      print(hunk)
+      local start = hunk.added.start
+      local last = start + hunk.added.count
+      -- nvim_buf_get_lines uses zero-based indexing -> subtract from last
+      local last_hunk_line = vim.api.nvim_buf_get_lines(0, last - 2, last - 1, true)[1]
+      local range = { start = { start, 0 }, ["end"] = { last - 1, last_hunk_line:len() } }
+      format({ range = range, async = false, lsp_fallback = true, quiet = true }, function()
+        vim.defer_fn(function()
+          format_range()
+        end, 1)
+      end)
+    end
+  end
+
+  format_range()
+end
+
+require("conform").setup({
+  -- options,
+  format_on_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      print("Auto format disabled!")
+      return
+    end
+    -- Prefer to format git hunks instead of the entire file
+    format_hunks()
+  end,
+})
 return M
