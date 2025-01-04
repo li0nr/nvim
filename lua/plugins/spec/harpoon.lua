ad86f4aba044493053e1eba86fc41d3c7bb66638
Ramy Fakhoury <Ramy.fakhoury@vastdata.com>
2025-01-04 16:11:15 +0200

    Harpoon: added ctrl-n/p to jump between pinned files.

--- lua/plugins/spec/harpoon.lua
+++ lua/plugins/spec/harpoon.lua
@@ -9,6 +9,8 @@ return {
   keys = {
     { "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with Harpoon" },
     { "<leader>he", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Toggle Harpoon menu" },
+    { "<C-n>",      "<cmd>lua require('harpoon.ui').nav_next()<cr>",          desc = "Next Harpoon mark" },
+    { "<C-p>",      "<cmd>lua require('harpoon.ui').nav_prev()<cr>",          desc = "Prev Harpoon mark" },
     { "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Harpoon mark 1" },
     { "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Harpoon mark 2" },
     { "<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Harpoon mark 3" },
